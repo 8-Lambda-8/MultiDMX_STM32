a594fe3e24b326d578e82e22d7d5ac8599c96ac6
8-Lambda-8 <j.wasle111@gmail.com>
2023-07-29 16:53:36 +0200

    added .h

---
+++ MultiDMX_STM32.h
@@ -0,0 +1,19 @@
+#ifndef MultiDmx_h
+#define MultiDmx_h
+
+#include "Arduino.h"
+
+#define DMX_SIZE 512
+
+#ifndef DMX_UNIVERSES
+#define DMX_UNIVERSES 2
+#endif
+
+class MultiDmxClass {
+ public:
+  void write(uint8_t universe, uint16_t channel, uint8_t value);
+  void usePins(uint8_t pins[DMX_UNIVERSES]);
+};
+extern MultiDmxClass MultiDmx;
+
+#endif
